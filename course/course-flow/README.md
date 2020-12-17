acedacad6d06824ebf7d0d6a73cde4af14153beb
BlasToth <balazstothlaszlo+github@gmail.com>
2020-12-17 15:45:02 +0100

    serving HTML pages

--- course/course-flow/README.md
+++ course/course-flow/README.md
@@ -1,3 +1,4 @@
 # Introduction to Express.js
 
 - filtering path
+- sendFile(path.join(__dirname, 'directory', 'file'));
--- course/course-flow/routes/shop.js
+++ course/course-flow/routes/shop.js
@@ -1,10 +1,13 @@
-const { Router } = require('express');
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>')
+    // __dirname = absolute path to our project folder
+    // path.join() will build the path both for Linux ('/user) and Windows ('\user) by detecting the operating system
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
