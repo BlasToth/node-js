42471fa7b00c88c9419ef9bce30452538e4df07a
BlasToth <balazstothlaszlo+github@gmail.com>
2020-12-27 12:10:39 +0100

    connecting to a mongo DB

--- course/mongo-db/00-setup/util/database.js
+++ course/mongo-db/00-setup/util/database.js
@@ -1,15 +1,28 @@
 const mongodb = require('mongodb');
+const { get } = require('../routes/admin');
 const MongoClient = mongodb.MongoClient;
 
-const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://user:password@cluster0-try.zhddr.mongodb.net/<dbname>?retryWrites=true&w=majority', { useUnifiedTopology: true })
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://@cluster0-try.zhddr.mongodb.net/shop?retryWrites=true&w=majority', { useUnifiedTopology: true })
   .then(client => {
     console.log('connected!'); 
-    callback(result);
+    _db = client.db();
+    callback();
   })
   .catch(err => {
     console.log(err);
+    throw err;
   });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!'
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
